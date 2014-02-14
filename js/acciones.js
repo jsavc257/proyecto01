592d585cbc834b0d83c20c8300bca8241b6ac1cc
jose saviñon cuellar <jsavc80@gmail.com>
2014-02-14 12:16:08 -0600

    agregar linea
    agregar vibrar

--- js/acciones.js
+++ js/acciones.js
@@ -4,5 +4,11 @@ $(document).ready(function() { // llave principal ready
          $('#B1').tap(function () { //lave tap
 		  navigator.notification.beep (1); //sonido en el disposito
     	 });//llave tap b1
+		 
+		 
+		 $('#V1').tap(function () { //lave tap2
+		  navigator.notification.vibrate (500); //vibrar en el disposito
+    	 });//llave tap2 v1
+		 
 	}); //llave del device ready
 }); // llave principal ready
